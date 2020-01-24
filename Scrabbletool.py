ef68b8105242f49de9ba6f427c4ad54a4d2e6be2
Simox-CU <58033350+Simox-CU@users.noreply.github.com>
2020-01-24 15:04:44 +0100

    Update Scrabbletool.py

--- Scrabbletool.py
+++ Scrabbletool.py
@@ -31,7 +31,6 @@ def score_mutacia():
 
 mutacia = ""
 
-##Počítanie skóre slov##
 def scrabble_score(word):
     score_cz,score_en,score_sk = score_mutacia()	
     global mutacia
@@ -48,6 +47,10 @@ def scrabble_score(word):
 		
     return sum(total)
 
+def readFile(filename):
+    return [i.strip('\n') for i in open(filename,encoding="utf8")]
+
+
 ##Zvolenie slovníka##
 def readDictionary():
     global mutacia
@@ -55,77 +58,61 @@ def readDictionary():
         d1a = input("Zadaj veľké písmeno zo zoznamu, ktorý slovník na hľadanie slov sa má použiť : \nA) slovenský (bez diakritiky) \nB) slovenský (s diakritikou) \nC) český (s diakritikou)\nD) anglický \nE) vlastný\n")
         mutacia = d1a
         if d1a == "A":
-            dictionary = [i.strip('\n') for i in open('zapoctovy_program_slovnik_sk.txt',encoding="utf8")]
-            break
+            return readFile('zapoctovy_program_slovnik_sk.txt')
         elif d1a == "B":
-            dictionary = [i.strip('\n') for i in open('zapoctovy_program_slovnik_sk_diakritika.txt',encoding="utf8")]
-            break
+            return readFile('zapoctovy_program_slovnik_sk_diakritika.txt')
         elif d1a == "C":
-            dictionary = [i.strip('\n') for i in open('zapoctovy_program_slovnik_cz_diakritika.txt',encoding="utf8")]
-            break
+            return readFile('zapoctovy_program_slovnik_cz_diakritika.txt')
         elif d1a == "D":
-            dictionary = [i.strip('\n') for i in open('zapoctovy_program_slovnik_en.txt',encoding="utf8")]
-            break
+            return readFile('zapoctovy_program_slovnik_en.txt')
         elif d1a == "E":
-            filename = input('Zadaj názov súboru vlastného slovníka vrátane pripony: ')
-            dictionary = [i.strip('\n') for i in open(filename,encoding="utf8")]
-            break
-    return dictionary
+            return readFile(input('Zadaj názov súboru vlastného slovníka vrátane pripony: '))
 
 ##Zadanie reťazca písmen##
 def readWord():
     word = input("Zadaj vstupný reťazec (malých) písmen: ").lower()
-
     while not word.isalpha():
         print("Zadaný reťazec musí obsahovať jedine písmená abecedy!")
         word = input("Zadaj znovu vstupný reťazec (malých) písmen: ").lower()
-
     return list(word)
 
 
 ##Vypísanie skóre a slov##
 def writePossibilities(items):
     nd_array = []
-
     for item in items:
         x=scrabble_score(item)
         nd_array.append(str(x) + " " + item)	
-
-    nd_array.sort(key=lambda x: int(''.join(filter(str.isdigit, x))),reverse=True)
-
+        nd_array.sort(key=lambda x: int(''.join(filter(str.isdigit, x))),reverse=True)
     for item in nd_array:
         print(item)
 
 ##Získavanie slov zo slovníku na základe zadaného reťazca##    
 def get_words(dictionary, orginal_s, new_s, word_list):
-    
     if not new_s:
        return word_list
     else:
         possibilities = [i for i in dictionary if all(new_s.count(b) >= i.count(b) for b in i)]
-        
         number_of_possibilities = len(possibilities)
-
-        if (number_of_possibilities == 0 ):     
+    if  number_of_possibilities == 0:     
             print("Na základe vstupného reťazca písmen",orginal_s, " nebolo nájdené v príslušnom slovníku žiadne slovo!")
-        else: 
+    else: 
           print("Na základe vstupného reťazca písmen",orginal_s, "bolo nájdených v príslušnom slovníku počet slov s príslušným score:",number_of_possibilities)
           print("Sú to tieto slová:")
           writePossibilities(possibilities)
-     
           if not possibilities:
-                return get_words(dictionary, orginal_s, [], word_list)
+            return get_words(dictionary, orginal_s, [], word_list)
           else:
-            word = random.choice(possibilities)
-            word_list.append(word)
-            word_dict = {i:word.count(i) for i in word}
-            new_final_word = list(itertools.chain.from_iterable([[a for i in range(abs(orginal_s.count(a)-b))] for a, b in word_dict.items()]))
-            return get_words(dictionary, orginal_s, new_final_word, word_list)
+              word = random.choice(possibilities)
+              word_list.append(word)
+              word_dict = {i:word.count(i) for i in word}
+              new_final_word = list(itertools.chain.from_iterable([[a for i in range(abs(orginal_s.count(a)-b))] for a, b in word_dict.items()]))
+              return get_words(dictionary, orginal_s, new_final_word, word_list)
 
-dict = readDictionary()
+d = readDictionary()
 while True:
     s = readWord()
-    final_words = get_words(dict, s, s, [])
+    final_words = get_words(d, s, s, [])
     answer = input("Chceš pokračovať vo vyhľadávaní ? Zadaj (a/n): ")
     if answer == 'n':
       break
